20a987923c4a40e4f3005f5462508e46847382e9
Stovencrantz <sknapp143@gmail.com>
2021-02-04 13:30:05 -0500

    Progressed the application so that the app can now navigate to the particular product after logging in, it will then check to see if the add-to-cart button is enabled or disabled. If disabled, it will notify the user, but if it enabled and clickable, it will proceed to click it and add the item to the users cart and then will navigate to the users cart. Next step is to set the applications so that if the button is found to be disabled, it will continuously loop and refresh the page until the button becomes active, indicating that stock has been dropped, and then proceed the truthly side of the if-else statement

--- production/bestBuyBot.py
+++ production/bestBuyBot.py
@@ -32,11 +32,13 @@ try:
 
         # Try/except to open account dropdown and click signIn button
     try:
-        signInMenu = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "account-menu-container"))
+        # signInMenu = WebDriverWait(driver, 10).until(
+        #     EC.presence_of_element_located((By.ID, "account-menu-container"))
+        # )
+        signInButton = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "lam-signIn__button"))
         )
         # signInButton = signInMenu.find_element_by_class_name("lam-signIn__button")
-        signInButton = signInMenu.find_element_by_class_name("lam-signIn__button")
         signInButton.click()
         # Try/except to fill out signin info and click SignIn button
         try: 
@@ -52,13 +54,54 @@ try:
             signInButtonFinal = signInForm.find_element_by_class_name("cia-form__controls__submit")
             signInButtonFinal.click()
 
+            # Try/except to certify that the user is signed in
             try:
                 loggedIn = WebDriverWait(driver, 10).until(
                  EC.presence_of_element_located((By.CLASS_NAME, "logged_user_name"))
             )
                 time.sleep(2)
                 print("COngrats! You are logged in")
+                # driver.get('https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442');
+                driver.get('https://www.bestbuy.com/site/logitech-z150-2-0-multimedia-speakers-2-piece-black/5326434.p?skuId=5326434');
+
+
                 time.sleep(2)
+
+                # Try/except to certify that the item is in stock, and if so to add it to the cart
+                try:
+                    addToCartButton = WebDriverWait(driver, 10).until(
+                    # EC.presence_of_element_located((By.XPATH, "//button[contains(@class,'add-to-cart-button') and contains(@class, 'btn-leading-ficon')]"))
+                    # EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'add-to-cart-button')]"))
+                    EC.presence_of_element_located((By.CLASS_NAME, 'add-to-cart-button'))
+                )
+
+                    # If the add to cart button is present, meaning the item is IN STOCK, click the button and proceed to check out
+                    if (addToCartButton.is_enabled()): 
+                        addToCartButton.click()                
+                        print("Looks like the item is in stock, We have added your item to the cart")
+                        # print("addToCartButton is enabled")
+                        time.sleep(2)                
+                        
+                        try:
+                            goToCartButton = WebDriverWait(driver, 10).until(
+                                EC.presence_of_element_located((By.CLASS_NAME, "go-to-cart-button"))
+                            )
+                            goToCartButton.click()                
+                            print("Navigating to our cart")
+                            time.sleep(2)
+                            
+                        except Exception as e: print(e)
+
+                    # If the add to cart button is NOT present, meaning the item is OUT OF STOCK, refresh the page
+                    else:
+                        # print("addToCartButton is disabled")
+                        print("Looks like our Item is out of stock, check back later!")
+                        # driver.get('https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442');
+                        driver.get('https://www.bestbuy.com/site/logitech-z150-2-0-multimedia-speakers-2-piece-black/5326434.p?skuId=5326434');
+
+
+                except Exception as e: print(e)
+
             except Exception as e: print(e)
             # except:
             #     print("Couldnt recognize logged user")
